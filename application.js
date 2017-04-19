d746f772a3f53f1ca2813962586c935c00f2d1d7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-19 16:15:23 -0400

    Lansdowne Place 1.0  /application.js

--- application.js
+++ application.js
@@ -435,10 +435,11 @@ function renderNewStores(container, template, collection){
         var today = moment();
         var store_opens = moment(val.new_store_open_date);
         if (today.tz(getPropertyTimeZone()) <= store_opens.tz(getPropertyTimeZone())) {
-            val.open = "Opens on " + moment(val.new_store_open_date).format("MMM DD");
-        } else {
-            val.open = "Now Open!"
+            val.open = "Opens " + moment(val.new_store_open_date).format("MMM DD");
         } 
+        // else {
+        //     val.open = "Now Open!"
+        // } 
         
         if (val.description.length  >= 70) {
             val.description = val.description.substring(0,69) + "...";
