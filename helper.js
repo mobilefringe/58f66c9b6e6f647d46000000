b5ca332caf2343f23115c370aebe0fd46b5109a2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-19 11:22:50 -0400

    Lansdowne Place 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,3 +1,13 @@
 function init() {
+    $('#back-top a').click(function () {
+        $('body,html').animate({
+        	scrollTop: 0
+        }, 800);
+        return false;
+    });
     
+    $(".menu-phone-btn").click(function(){
+        $(".menu-phone-list").slideToggle("fast");
+    });
+
 }
