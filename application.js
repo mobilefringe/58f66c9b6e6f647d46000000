87a969f217a951ff44adffc3493bc9821fa06b12
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-19 16:48:03 -0400

    Lansdowne Place 1.0  /application.js

--- application.js
+++ application.js
@@ -446,8 +446,8 @@ function renderNewStores(container, template, collection){
             val.show = "display: none;";
         }
 
-        if (val.description.length  >= 100) {
-            val.description = val.description.substring(0, 99) + "...";
+        if (val.description.length  >= 200) {
+            val.description = val.description.substring(0, 199) + "...";
         }
         var repo_rendered = Mustache.render(template_html,val);
         item_rendered.push(repo_rendered);
