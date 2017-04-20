e59e0a908c72afaa89293d818baaeffeddc26ea0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-20 16:28:10 -0400

    Lansdowne Place 1.0  /application.js

--- application.js
+++ application.js
@@ -392,7 +392,7 @@ function renderPromoDetails(container, template, collection){
             val.dates = start.format("MMM D")
         }
         else{
-            val.dates = "Starts " + start.format("MMM D") + " - Ends " + end.format("MMM D")
+            val.dates = "STARTS " + start.format("MMM D") + " - ENDS " + end.format("MMM D")
         }
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
