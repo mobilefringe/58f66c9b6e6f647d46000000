d77a3d6a0b4298e34cd45785450b75515a2c2dec
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-21 13:28:58 -0400

    Lansdowne Place 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,6 +1,23 @@
 function init() {
     $('<div class="modal-backdrop custom_backdrop"><div class="loader">Loading...</div></div>').appendTo(document.body);
     
+    if($(window).width() >= 768) {
+        var top = $('#header').offset().top - parseFloat($('#header').css('marginTop').replace(/auto/, 100));
+        $(window).scroll(function (event) {
+            var y = $(this).scrollTop();
+            if (y >= top) {
+                $('#header').addClass('fixed');
+            } else {
+                $('#header').removeClass('fixed');
+            }
+            if (y > 300) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+    }
+
     $('#back-top a').click(function () {
         $('body, html').animate({
         	scrollTop: 0
