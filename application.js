461c42ffa4c67ea00d611c97be3008cc9d1353a8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-21 09:44:49 -0400

    Lansdowne Place 1.0  /application.js

--- application.js
+++ application.js
@@ -81,12 +81,12 @@ function renderEventDetails(container, template, collection){
             var store_details = getStoreDetailsByID(val.eventable_id);
             val.store_detail_btn = store_details.slug;
             val.store_name = store_details.name;
-            if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
-                val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
-            }
-            else{
-                val.image_url = store_details.store_front_url_abs;
-            }
+            // if (store_details.store_front_url_abs.indexOf('missing.png') > -1){
+            //     val.image_url = "//codecloud.cdn.speedyrails.net/sites/57f66e416e6f6465fe050000/image/jpeg/1446753494000/Dixie_default.jpg";
+            // }
+            // else{
+            //     val.image_url = store_details.store_front_url_abs;
+            // }
         }
         else{
             val.store_name = "Dixie Outlet";
