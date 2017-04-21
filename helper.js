dbf10eb22b35d19668d8cb9e189bf2d5cb30a3d6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-21 13:29:50 -0400

    Lansdowne Place 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,6 +1,21 @@
 function init() {
     $('<div class="modal-backdrop custom_backdrop"><div class="loader">Loading...</div></div>').appendTo(document.body);
     
+    if($(window).width() >= 768 && $(window).width() <= 1024) {
+      $('meta[name=viewport]').attr('content','width=980, user-scalable=no');
+    }
+    
+    $('#back-top a').click(function () {
+        $('body,html').animate({
+        	scrollTop: 0
+        }, 800);
+        return false;
+    });
+    
+    $(".menu-phone-btn").click(function(){
+        $(".menu-phone-list").slideToggle("fast");
+    });
+                
     if($(window).width() >= 768) {
         var top = $('#header').offset().top - parseFloat($('#header').css('marginTop').replace(/auto/, 100));
         $(window).scroll(function (event) {
@@ -18,16 +33,6 @@ function init() {
         });
     }
 
-    $('#back-top a').click(function () {
-        $('body, html').animate({
-        	scrollTop: 0
-        }, 800);
-        return false;
-    });
-    
-    $(".menu-phone-btn").click(function(){
-        $(".menu-phone-list").slideToggle("fast");
-    });
 
 }
 
