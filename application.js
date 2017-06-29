4a7d0cc10a3bbce17abe2455f26a4b8a784a6653
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 11:21:55 -0400

    Lansdowne Place 1.0  /application.js

--- application.js
+++ application.js
@@ -322,7 +322,7 @@ function renderStoreHours(container, template, collection, type){
     }
     if (type == "holiday_hours") {
         $.each( collection , function( key, val ) {
-            if (!val.store_id && val.is_holiday == true) {
+            if (val.is_holiday == true) {
                 holiday = moment(val.holiday_date);
                 val.formatted_date = holiday.format("MM/DD/YY");
                 if (val.open_time && val.close_time && val.is_closed == false){
