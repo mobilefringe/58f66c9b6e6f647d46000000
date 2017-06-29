d8cdca58c0475c48129bf5da499f8a6801e7fc31
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-06-29 11:20:22 -0400

    Lansdowne Place 1.0  /application.js

--- application.js
+++ application.js
@@ -211,6 +211,7 @@ function renderHours(container, template, collection, type){
     Mustache.parse(template_html);   // optional, speeds up future uses
     if (type == "reg_hours") {
         $.each( collection , function( key, val ) {
+             console.log("yo sup",val);
             if (!val.store_id && val.is_holiday === false) {
                 switch(val.day_of_week) {
                     case 0:
