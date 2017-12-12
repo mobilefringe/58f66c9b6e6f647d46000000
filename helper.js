3e317fa5bd0968f5ff09c8657331faf95b83b316
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-12 11:56:53 -0500

    Lansdowne Place 1.1  /helper.js

---
+++ helper.js
@@ -0,0 +1,150 @@
+function init() {
+    $('<div class="modal-backdrop custom_backdrop"><div class="loader">Loading...</div></div>').appendTo(document.body);
+    
+    if($(window).width() >= 768 && $(window).width() <= 1024) {
+      $('meta[name=viewport]').attr('content','width=980, user-scalable=no');
+    }
+    
+    $('#back-top a').click(function () {
+        $('body,html').animate({
+        	scrollTop: 0
+        }, 800);
+        return false;
+    });
+    
+    $(".menu-phone-btn").click(function(){
+        $(".menu-phone-list").slideToggle("fast");
+    });
+                
+    if($(window).width() >= 768) {
+        var top = $('#header').offset().top - parseFloat($('#header').css('marginTop').replace(/auto/, 100));
+        $(window).scroll(function (event) {
+            var y = $(this).scrollTop();
+            if (y >= top) {
+                $('#header').addClass('fixed');
+            } else {
+                $('#header').removeClass('fixed');
+            }
+            if (y > 300) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+    }
+    
+    $('#subForm').submit(function(e) {
+        if ($("#agree_terms").prop("checked") != true){
+            alert("Please agree to the term and conditions.");
+            $("#agree_terms").focus();
+            return false;
+        }
+        e.preventDefault();
+        $.getJSON(
+            this.action + "?callback=?",
+            $(this).serialize(),
+            function (data) {
+                if (data.Status === 400) {
+                    alert("Please try again later.");
+                } else { // 200
+                    window.location.href = '/pages/lansdowne-thank-you';
+                }
+            });
+    });
+    
+    blog_searcher();
+    
+    //dynamically changing copyright year
+var current_year = moment().year();
+$("#current_year").text(current_year);
+}
+
+function show_content(){
+    $(".yield").css({visibility: "visible"});
+    $(".modal-backdrop").remove();
+}
+
+function blog_searcher(){
+    $('#close_search').click(function(){
+        $(this).hide();
+        $('#search_results_stores').html('');
+        $('#search_results_events').html('');
+        $('#search_results_promotions').html('');
+        $('#search_results_stores').hide();
+        $('#search_results_events').hide();
+        $('#search_results_promotions').hide();
+        $('#site_search').val('')
+    });
+    $('#site_search').keyup(function(){
+        if ($('#site_search').val() == ""){
+            $('#search_results_stores').html('');
+            $('#search_results_events').html('');
+            $('#search_results_promotions').html('');
+            $('#search_results_stores').hide();
+            $('#search_results_events').hide();
+            $('#search_results_promotions').hide();
+            $('#close_search').hide();
+        }
+        else{
+            $('#close_search').show();
+            $('#search_results_stores').html('');
+            $('#search_results_events').html('');
+            $('#search_results_promotions').html('');
+            
+            var val = $(this).val().toLowerCase();
+            
+            results = getSearchResults(val);
+            var s_stores = results.stores;
+            var s_events = results.events;
+            var s_promos = results.promotions;
+            
+            if(s_stores !=undefined && s_stores.length > 0){
+                var h2_stores = "<h2 id='open_stores' class='li_open'>(" +s_stores.length + ") Stores<i class='pull-right fa fa-chevron-down'></i></h2>";
+                $('#search_results_stores').append(h2_stores);
+                $.each(s_stores, function(i, v){
+                    var div_stores = "<div class='blog_search_results collapse_open_stores'>";
+                    div_stores = div_stores + "<h4><a href='/stores/" + v.slug + "'>" + v.name + "</a></h4>";
+                    div_stores = div_stores + "</div>";
+                    $('#search_results_stores').append(div_stores);
+                    $('#search_results_stores').show();
+                });
+            }
+            if(s_promos != undefined && s_promos.length > 0){
+                var h2_promotions = "<h2 id='open_promotions' class='li_open'>(" +s_promos.length + ") Promotions<i class='pull-right fa fa-chevron-down'></i></h2>";
+                $('#search_results_promotions').append(h2_promotions);
+                $.each(s_promos, function(i, v){
+                    var div = "<div class='blog_search_results collapse_open_promotions'>";
+                    div = div + "<h4><a href='/promotions/" + v.slug + "'>" + v.name + "</a></h4>";
+                    div = div + "</div>";
+                    $('#search_results_promotions').append(div);
+                    $('#search_results_promotions').show();
+                });
+            }   
+            if(s_events != undefined && s_events.length > 0){
+                var h2_events = "<h2 id='open_events' class='li_open'>(" +s_events.length + ") Events<i class='pull-right fa fa-chevron-down'></i></h2>";
+                $('#search_results_stores').append(h2_events);
+                $.each(s_events, function(i, v){
+                    var div = "<div class='blog_search_results collapse_open_events'>";
+                    div = div + "<h4><a href='/events/" + v.slug + "'>" + v.name + "</a></h4>";
+                    div = div + "</div>";
+                    $('#search_results_stores').append(div);
+                    $('#search_results_stores').show();
+                });
+            }
+
+            $('.li_open').click(function(){
+                var collapse = ".collapse_" + $(this).attr('id');
+                if($(this).hasClass('open')){
+                    $(collapse).slideUp('fast');
+                    $(this).removeClass('open');
+                }
+                else{
+                    $(this).addClass('open');
+                    $(collapse).slideDown('fast');
+                }
+                
+            })
+            
+        }
+    });
+}
